a1be1ea164e97408b8fa5428872a0a2cee1ca7fc
Hyojeong <79013520+world-dv@users.noreply.github.com>
2024-01-23 18:00:30 +0900

    Create 성분으로 구분한 아이스크림 총 주문량.sql

---
+++ GROUP BY/성분으로 구분한 아이스크림 총 주문량.sql
@@ -0,0 +1,5 @@
+SELECT i.INGREDIENT_TYPE as INGREDIENT_TYPE, sum(f.TOTAL_ORDER) as TOTAL_ORDER
+    from FIRST_HALF f left join ICECREAM_INFO i on (f.FLAVOR = i.FLAVOR)
+    group by i.INGREDIENT_TYPE
+    order by TOTAL_ORDER
+    
