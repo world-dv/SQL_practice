835bf2c7ee757d97e8287e8d5f877ec6a284430f
Hyojeong <79013520+world-dv@users.noreply.github.com>
2024-02-16 21:36:02 +0900

    Create 업그레이드 할 수 없는 아이템 구하기.sql

---
+++ IS NULL/업그레이드 할 수 없는 아이템 구하기.sql
@@ -0,0 +1,6 @@
+select ITEM_ID, ITEM_NAME, RARITY
+    from ITEM_INFO
+    where ITEM_ID not in (
+        select distinct PARENT_ITEM_ID from ITEM_TREE where PARENT_ITEM_ID is not NULL
+    )
+    order by ITEM_ID desc
